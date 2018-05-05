4fb6a1f1896ae4725b9cccd8e3a79f84fe23ff21
Yaugen Zhuk <yaugen.zhuk@gmail.com>
2018-05-05 20:49:25 +0300

    Minimal file arch has been created.

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
 
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="Video-Inpainting",
     version="0.1.1",
     packages=find_packages(),
-
     package_data={
         '': ['*.txt']
     },
     author="Yaugen Zhuk",
     author_email="yaugen.zhuk@gmail.com",
     description="Video Inpainting Algorithm",
+    long_description=long_description,
     license="MIT",
 
 )
